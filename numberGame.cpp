c3811b29c275dcdd0f9cb99015e082e08a35e4a7
Devin Jhu <60712826+devin-jhu@users.noreply.github.com>
2022-05-19 17:14:53 -0400

    Update numberGame.cpp

--- numberGame.cpp
+++ numberGame.cpp
@@ -10,15 +10,15 @@
 
 int main() {
     // this function is a guessing game
-    int NUMBER_ANSWER;
+    int guess;
+    int numberAnswer;
 
     std::random_device rseed;
     // mersenne_twister
     std::mt19937 rgen(rseed());
-    // randome number between [1,9]
+    // random number between [1,9]
     std::uniform_int_distribution <int> idist(1 , 9);
-    NUMBER_ANSWER = idist(rgen);
-    int GUESS;
+    numberAnswer = idist(rgen);
 
     std::cout << "The number game";
     std::cout << "" << std::endl;
@@ -26,14 +26,14 @@ int main() {
     // input
     std::cout << "" << std::endl;
     std::cout << "enter number: ";
-    std::cin >> GUESS;
+    std::cin >> guess;
 
     // process
-    if (GUESS == NUMBER_ANSWER) {
+    if (guess == numberAnswer) {
         std::cout << "You win!";
     } else {
         std::cout << "You lose :(";
     }
     std::cout << "" << std::endl;
-    std::cout << "Done.";
+    std::cout << "\nDone.";
 }
